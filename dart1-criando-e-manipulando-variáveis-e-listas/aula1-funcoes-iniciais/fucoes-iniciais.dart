3a23ace3a105f2f54df6a1ab39fd510b417605bc
Thiago Izidoro Silva Santos <thiagoiss@estudante.ufscar.br>
2023-02-25 07:33:25 -0300

    primeira aula

---
+++ dart1-criando-e-manipulando-variáveis-e-listas/aula1-funcoes-iniciais/fucoes-iniciais.dart
@@ -0,0 +1,7 @@
+void main() {
+  int idade = 36;
+  double altura = 1.86;
+  bool geek = true;
+  String nome = 'Thiago';
+  print('Meu nome é $nome \n e tenho $idade anos, $altura m. \n Você gosta da cultura geek? $geek');
+}
