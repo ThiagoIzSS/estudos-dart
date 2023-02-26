91b6ba25eb010368351b29423c441ef132955c91
Thiago Izidoro Silva Santos <thiagoiss@estudante.ufscar.br>
2023-02-26 10:26:24 -0300

    Mais um dia de estudo dart

---
+++ dart1-criando-e-manipulando-variáveis-e-listas/aula5-for.dart
@@ -0,0 +1,25 @@
+void main() {
+  int idade = 27;
+  double altura = 1.86;
+  bool geek = true;
+  const String nome = 'Caio Couto';
+  final String apelido;
+
+  apelido = 'Kako';
+
+  bool maiorDeIdade;
+  if (idade >= 18) {
+    maiorDeIdade = true;
+  } else {
+    maiorDeIdade = false;
+  }
+
+  for (var i = 1; i < 6; i++) {
+    print('concluir $i voltas');
+  }
+
+  List<dynamic> kako = [idade, altura, geek, nome, apelido];
+
+  print(
+      'Eu sou ${kako[4]}, mas meu nome completo é ${kako[3]}. Eu me considero geek? ${kako[2]}.\n Eu tenho ${kako[1]} m e ${kako[0]} anos. Eu sou maior de idade? ${maiorDeIdade}');
+}
