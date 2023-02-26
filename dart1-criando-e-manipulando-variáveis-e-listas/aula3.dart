93a48f6259e64645eee2d6bd0a898adcf78b9707
Thiago Izidoro Silva Santos <thiagoiss@estudante.ufscar.br>
2023-02-25 22:26:26 -0300

    continua estudos

---
+++ dart1-criando-e-manipulando-variáveis-e-listas/aula3.dart
@@ -0,0 +1,6 @@
+List<dynamic> thiago = [36, 1.76, 'Thiago Izidoro', true,];
+
+void main() {
+  print(thiago);
+  print('Meu nome é ${thiago[2]}, tenho ${thiago[1]} de altura, ${thiago[0]} anos. é ${thiago[3]} que eu gosto de ler');
+}
